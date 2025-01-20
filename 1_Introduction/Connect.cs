4828c267ac4afe00ddb8a648d8ecbb2f74124123
kaushambi.gujral <kaushambi.gujral@eroad.com>
2025-01-19 19:16:43 -0800

    moved class to its own file

---
+++ 1_Introduction/Connect.cs
@@ -0,0 +1,23 @@
+using MongoDB.Bson;
+using MongoDB.Driver;
+
+public class Connect
+{            
+  // Update db password in the connection string
+  private const string MongoConnectionString = "mongodb+srv://kaushambi2612:<db_password>@cluster0.kmmzz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+  public static void PingCluster()
+  {
+    Console.WriteLine("Attempting to connect to Atlas cluster...");
+
+    // Connect to your Atlas cluster via a MongoClient
+    var client = new MongoClient(MongoConnectionString);
+
+    // Send a ping to confirm a successful connection
+    try {
+        var result = client.GetDatabase("admin").RunCommand<BsonDocument>(new BsonDocument("ping", 1));
+        Console.WriteLine("Successfully connected to Atlas");
+    } 
+    catch (Exception e) { Console.WriteLine(e);}
+  }
+}
--- 1_Introduction/Program.cs
+++ 1_Introduction/Program.cs
@@ -1,23 +1,4 @@
-﻿using MongoDB.Bson;
-using MongoDB.Driver;
+﻿// Uncomment to run a method
 
-public class Connect
-{            
-  // Update db password in the connection string
-  private const string MongoConnectionString = "mongodb+srv://kaushambi2612:<db_password>@cluster0.kmmzz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
-
-  public static void Main(string[] args)
-  {
-    Console.WriteLine("Attempting to connect to Atlas cluster...");
-
-    // Connect to your Atlas cluster via a MongoClient
-    var client = new MongoClient(MongoConnectionString);
-
-    // Send a ping to confirm a successful connection
-    try {
-        var result = client.GetDatabase("admin").RunCommand<BsonDocument>(new BsonDocument("ping", 1));
-        Console.WriteLine("Successfully connected to Atlas");
-    } 
-    catch (Exception e) { Console.WriteLine(e);}
-  }
-}
+// a. Connecting to an atlas cluster
+Connect.PingCluster();
